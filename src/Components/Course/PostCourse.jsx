e9736fcd60d25aeea38382c193cf10c4a493c6ea
Iftikher Lutfur <iftikherlutfur@gmail.com>
2024-12-29 14:32:34 +0600

    implement the course post route and it is working dynamically

--- src/Components/Course/PostCourse.jsx
+++ src/Components/Course/PostCourse.jsx
@@ -1,50 +1,161 @@
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useContext, useState } from "react";
+import { AuthContext } from "../AuthProvider/AuthProvider";
+
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`
 
 const PostCourse = () => {
-    return (
-        <div>
-            <section className="p-6 dark:bg-gray-100 pt-20 dark:text-gray-900">
-	<form noValidate="" action="" className="container flex flex-col mx-auto space-y-12 bg-black bg-opacity-50">
-		<fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm dark:bg-gray-50">
-			<div className="space-y-2 col-span-full lg:col-span-1">
-				<p className="font-medium text-white">CREATE A COURSE POST</p>
-				<p className=" text-white">Write a post about your course. mentioned the duration of your course and amount of this course also mentioned what is the main features of your course and what are you going to learn in this course. and write a description of your course.</p>
-			</div>
-			<div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
-				<div className="col-span-full sm:col-span-3">
-					<label htmlFor="firstname" className="text-sm">First name</label>
-					<input id="firstname" type="text" placeholder="First name" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
-				</div>
-				<div className="col-span-full sm:col-span-3">
-					<label htmlFor="lastname" className="text-sm">Last name</label>
-					<input id="lastname" type="text" placeholder="Last name" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
-				</div>
-				<div className="col-span-full sm:col-span-3">
-					<label htmlFor="email" className="text-sm">Email</label>
-					<input id="email" type="email" placeholder="Email" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
-				</div>
-				<div className="col-span-full">
-					<label htmlFor="address" className="text-sm">Address</label>
-					<input id="address" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
-				</div>
-				<div className="col-span-full sm:col-span-2">
-					<label htmlFor="city" className="text-sm">City</label>
-					<input id="city" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
-				</div>
-				<div className="col-span-full sm:col-span-2">
-					<label htmlFor="state" className="text-sm">State / Province</label>
-					<input id="state" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
-				</div>
-				<div className="col-span-full sm:col-span-2">
-					<label htmlFor="zip" className="text-sm">ZIP / Postal</label>
-					<input id="zip" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
-				</div>
-			</div>
-		</fieldset>
-		
-	</form>
-</section>
-        </div>
-    );
+
+	const [profilePhoto, setProfilePhoto] = useState(null);
+	// const navigate = useNavigate()
+
+	const uploadImageToImgbb = async (imageFile) => {
+		const formData = new FormData();
+		formData.append("image", imageFile);
+		try {
+			const response = await axios.post(image_hosting, formData, {
+				headers: { "Content-Type": "multipart/form-data" },
+			});
+			return response.data.data.display_url; // URL of the uploaded image
+		} catch (error) {
+			console.error("Image upload failed:", error);
+			throw new Error("Failed to upload image. Please try again.");
+		}
+	};
+
+	const { user } = useContext(AuthContext)
+	console.log(user.email);
+
+	const { register, handleSubmit } = useForm({
+		shouldUseNativeValidation: true
+	})
+
+	const onSubmit = async (data) => {
+		console.log(data);
+		const profilePhotoUrl = await uploadImageToImgbb(profilePhoto);
+
+		const courseInfo = {
+			courseName: data.courseName,
+			image: profilePhotoUrl,
+			email: data.email,
+			number:data.number,
+			description: data.description,
+			duration: data.courseDuration,
+			prerequisites: data.prefer,
+			targetAudience: data.targetAudience,
+			
+
+
+		}
+
+
+		try {
+			const res = await axios.post('http://localhost:5000/coursePost', courseInfo)
+			console.log(res.data);
+			if (res.data.insertedId) {
+				// navigate('/getCourse')
+			}
+			else{
+				console.error("Error")
+			}
+		} catch (error) {
+			console.error("Course upload error", error)
+		}
+
+
+	}
+
+	return (
+		<div>
+			<section className="max-w-4xl pt-20 p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
+				<h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">Create a course post</h2>
+
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+
+						{/* Course Name field */}
+						<div>
+							<label className="block text-sm font-medium" htmlFor="Course name">Course Name</label>
+
+							<input type="text" name="courseName" {...register("courseName", {
+								required: "Type your name"
+							})} className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+						</div>
+						{/* Course Banner Field*/}
+						<div>
+							<fieldset className="w-full space-y-1 dark:text-gray-800">
+								<label htmlFor="files" className="block text-sm font-medium">Attachments</label>
+								<div className="flex">
+									<input type="file" name="image" {...register("image", {
+										required: "Please upload your course banner"
+									})} onChange={(e) => setProfilePhoto(e.target.files[0])} className=" block w-full px-4 py-2 mt-2 text-gray-700 bg-white  border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring border-2 border-dashed " />
+								</div>
+							</fieldset>
+						</div>
+						{/* Course Duration Field */}
+						<div>
+							<label className="block text-sm font-medium" htmlFor="emailAddress">Course Duration</label>
+							<input type="text" name="courseDuration" {...register("courseDuration", {
+								required: "Please type your course duration"
+							})} className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+						</div>
+
+						<div>
+							<label className="block text-sm font-medium" htmlFor="password">Amount</label>
+							<input type="text" name="amount" {...register("amount", {
+								required: "Type your course amount"
+							})} className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+						</div>
+
+						<div>
+							<label className="block text-sm font-medium" htmlFor="passwordConfirmation">
+								Prerequisites</label>
+							<input type="text" name="prefer" {...register("prefer", {
+								required: "Please mention who will perefer for this course"
+							})} className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+						</div>
+						<div>
+							<label className="block text-sm font-medium" htmlFor="passwordConfirmation">
+								Target Audience</label>
+							<input type="text" name="targetAudience" {...register("targetAudience", {
+								required: "Type the target audience"
+							})} className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+						</div>
+						<div>
+							<label className="block text-sm font-medium" htmlFor="passwordConfirmation">
+								Email</label>
+							<input type="email" name="email" {...register("email", {
+								required: "Type your course email"
+							})} className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+						</div>
+						<div>
+							<label className="block text-sm font-medium" htmlFor="passwordConfirmation">
+								Number</label>
+							<input type="number" name="number" {...register("number", {
+								required: "Type your number where student get all the details about this course"
+							})} className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+						</div>
+
+					</div>
+
+					<div>
+						<label className="block text-sm font-medium" htmlFor="passwordConfirmation">Course Description</label>
+						<input type="text" name="description" {...register("description", {
+							required: "Type your course description"
+						})} className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+					</div>
+					<div className="flex justify-end mt-6">
+						<button type="submit" className="px-8 py-2.5 leading-5 w-full text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Post</button>
+					</div>
+
+
+				</form>
+			</section>
+
+		</div>
+	);
 };
 
 export default PostCourse;
